4ff46adf871f6ae87905141eac17ac8bedb01d55
Alexey Kushnir <al7787@gmail.com>
2024-06-10 18:59:15 +0300

    Updates

---
+++ assets/Home-DBwwYdHL.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-dmjrVXy8.js";const r=()=>e.jsx("h3",{children:"Home Page"});export{r as default};
---
+++ assets/Tweets-BbLNti7k.js
@@ -0,0 +1,12 @@
+import{p as e,L as t,j as o}from"./index-dmjrVXy8.js";const r=e(t)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+
+  &:hover,
+  &:focus {
+    color: #471ca9;
+  }
+`,s=()=>o.jsx("div",{children:o.jsx(r,{to:"/home",children:"Back to Home"})});export{s as default};
