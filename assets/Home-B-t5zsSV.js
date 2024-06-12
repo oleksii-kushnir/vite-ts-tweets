ff762b8922c8d393e2f50211e3d96a4966306e70
Alexey Kushnir <al7787@gmail.com>
2024-06-12 11:56:38 +0300

    Updates

---
+++ assets/Home-B-t5zsSV.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BrlRdteU.js";const r=()=>e.jsx("h3",{children:"Home Page"});export{r as default};
--- assets/Home-DBwwYdHL.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-dmjrVXy8.js";const r=()=>e.jsx("h3",{children:"Home Page"});export{r as default};
