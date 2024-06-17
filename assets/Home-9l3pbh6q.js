2b6346dee9741fd02b7411e71d0e75c67eb92c91
Alexey Kushnir <al7787@gmail.com>
2024-06-17 13:52:20 +0300

    Updates

--- assets/Home-9l3pbh6q.js
+++ assets/Home-9l3pbh6q.js
@@ -1,3 +1,3 @@
-import{p as t,j as e}from"./index-Durqdq7d.js";const o=t.h3`
+import{p as t,j as e}from"./index-DQCNz5se.js";const o=t.h3`
   text-align: center;
 `,s=()=>e.jsx(o,{children:"Home Page"});export{s as default};
