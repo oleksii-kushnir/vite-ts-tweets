2dc4dd478035761484050f6ab436220345260b43
Alexey Kushnir <al7787@gmail.com>
2024-06-18 13:51:45 +0300

    Updates

--- assets/Home-BNQ1bQJJ.js
+++ assets/Home-BNQ1bQJJ.js
@@ -1,3 +1,3 @@
-import{p as t,j as e}from"./index-DKchrWKe.js";const o=t.h3`
+import{p as t,j as e}from"./index-BkW5l96q.js";const o=t.h3`
   text-align: center;
 `,s=()=>e.jsx(o,{children:"Home Page"});export{s as default};
