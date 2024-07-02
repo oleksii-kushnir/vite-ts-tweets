203f054ceb8bf90996de338e7c2c06f8b981cd79
Alexey Kushnir <al7787@gmail.com>
2024-07-02 13:44:28 +0300

    Updates

--- assets/Home-CmMnZU7D.js
+++ assets/Home-CmMnZU7D.js
@@ -1,3 +1,3 @@
-import{p as t,j as e}from"./index-D7duGi1Q.js";const o=t.h3`
+import{p as t,j as e}from"./index-DYmSKcWI.js";const o=t.h3`
   text-align: center;
 `,s=()=>e.jsx(o,{children:"Home Page"});export{s as default};
